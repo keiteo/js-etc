dae71e16546804b0e62573175b8e461eedcc0e19
Lucria <jerryzhang28eclipse@gmail.com>
2020-10-31 12:11:53 +0800

    [Feat] Intial Code out for lunch

--- sample-bot.py
+++ sample-bot.py
@@ -41,11 +41,135 @@ def write_to_exchange(exchange, obj):
 def read_from_exchange(exchange):
     return json.loads(exchange.readline())
 
+# ~~~~~============== SYMBOLS TRACKER ==============~~~
+bond = []
+valbz = []
+vale = []
+gs = []
+ms = []
+wfc = []
+xlf = []
+order_id = 0
 
+# ~~~~~============== Execution Code ==============~~~~~
+def executeOrder(symbol, direction, price, size, exchange): # Direction is BUY or SELL
+    global order_id
+    jsonObject = {
+        "type": "add",
+        "order_id": order_id,
+        "symbol": symbol,
+        "dir": direction,
+        "price": price,
+        "size": size
+    }
+    print("JSON OBJECT IS ", jsonObject)
+    write_to_exchange(exchange, jsonObject) 
+    order_id += 1
+    # TODO handle accept and reject
+
+def executeConvert(symbol, direction, size):
+    global order_id
+    
+    jsonObject = {
+        "type": "convert",
+        "order_id": order_id,
+        "symbol" : symbol,
+        "dir": direction,
+        "size": size
+    }
+    write_to_exchange(exchange, jsonObject)
+    print("Convert for order ", order_id)
+    order_id += 1
+    
+def executeCancel(id):
+    jsonObject = {
+        "type": "cancel",
+        "order_id", id
+    } 
+    write_to_exchange(exchange, jsonObject)
+    print("Cancelled order ", id)
+    
+# ~~~~~============== Trading strats ==============~~~~~
+
+# if BUY orders > fairvalue, sell as much as possible
+# conversely for SELL orders
+def bondStrat1(message, exchange):
+    fairValue = 1000
+    buyArray = message["buy"]
+    sellArray = message.["sell"]
+    buyOrders = {"size": 0, "price":0}
+    sellOrders = {"size": 0, "price": 0}
+    for order in buyArray:
+        if order[0] > fairValue:
+            sellOrders["size"] += order[1]
+            sellOrders["price"] = min(sellOrders["price"], order[0])
+
+    for order in sellArray:
+        if order[0] < fairValue:
+            buyOrders["size"] += order[1]
+            buyOrders["price"] = max(sellOrders["price"], order[0])
+    
+    # buyOrder.size == 0 XOR sellOrder.size (should be)
+    if (sellOrder["size"] > 0):
+         executeOrder("BOND", "SELL", sellOrder["price"], sellOrder["size"], exchange)
+    elif (sellOrder["size"] > 0):
+        executeOrder("BOND", "BUY", buyOrder["price"], buyOrder["size"], exchange)
+
+def handleBonds(message, exchange):
+    bondStrat1(message, exchange)
+    
+    
+
+# receives a "book" message to see prices
+# message format: {"type":"book","symbol":"SYM","buy":[[PRICE,SIZE], ...],"sell":[...]}
+def handleBook(message, exchange):
+    if (message.symbol == "BOND"):
+        handleBonds(message, exchange)
+    elif (message.symbol =="VALBZ"):
+    elif (message.symbol =="VALE":
+    elif (message.symbol =="GS"):
+    elif (message.symbol =="MS"):
+    elif (message.symbol =="WFC"):
+    elif (message.symbol =="XLF"):
+
+# ~~~~~============== DATA EXTRACTION CODE ==============~~~
+
+# Read trade message and get current valuation for different symbols
+def getCurrentValuation(message):
+    symbol = message["symbol"]
+    if symbol == "BOND":
+        bond.append(message["price"])
+    elif symbol == "VALBZ":
+        valbz.append(message["price"])
+    elif symbol == "VALE":
+        vale.append(message["price"])
+    elif symbol == "GS":
+        gs.append(message["price"])
+    elif symbol == "MS":
+        ms.append(message["price"])
+    elif symbol == "WFC":
+        wfc.append(message["price"])
+    elif symbol == "XLF":
+        xlf.append(message["price"])
+    else:
+        print("Unknown symbol while getting current valuation")
+
+# Extract market data from message
+def handleMessage(message, exchange):
+    type = message["type"]
+    
+    if type == "book":
+        handleBook(message, exchange)
+    elif type == "trade":
+        getCurrentValuation(message)
+    
+
+        
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
 def main():
     exchange = connect()
+
     write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
     hello_from_exchange = read_from_exchange(exchange)
     # A common mistake people make is to call write_to_exchange() > 1
@@ -58,6 +182,8 @@ def main():
         if(message["type"] == "close"):
             print("The round has ended")
             break
+        
+        handleMessage(message, exchange)
 
 if __name__ == "__main__":
     main()
